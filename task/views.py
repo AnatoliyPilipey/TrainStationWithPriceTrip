8f3fce26b65bd14a25f6844d4514ad1746ffca42
Anatoliy Pilipey <pilipeytolya@gmail.com>
2024-01-10 18:33:34 +0200

    Add in task\views.py TrainTypeViewSet

--- task/views.py
+++ task/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from task.models import (
+    TrainType,
+    Train,
+    Journey,
+    Crew,
+    Route,
+    Station,
+    Ticket,
+    Order,
+)
+from task.serializers import (
+    TrainTypeSerializer,
+)
 
-# Create your views here.
+
+class TrainTypeViewSet(viewsets.ModelViewSet):
+    queryset = TrainType.objects.all()
+    serializer_class = TrainTypeSerializer
+
