dee7b3e153148a9ecfb1502103aec60fc187d033
Anatoliy Pilipey <pilipeytolya@gmail.com>
2024-01-10 18:44:04 +0200

    Add in task\urls.py router for train_type

--- task/urls.py
+++ task/urls.py
@@ -1,8 +1,14 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from task.views import (
+    TrainTypeViewSet,
+)
+
+router = routers.DefaultRouter()
+router.register("train_type", TrainTypeViewSet)
 
 
-urlpatterns = [
+urlpatterns = [path("", include(router.urls))]
 
-]
 
 app_name = "task"
