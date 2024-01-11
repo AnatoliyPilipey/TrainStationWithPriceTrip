6e893d9b844525d16661a3e995b6327b2a1f1979
Anatoliy Pilipey <pilipeytolya@gmail.com>
2024-01-11 21:04:36 +0200

    Add mode permission_off

--- task/permissions.py
+++ task/permissions.py
@@ -1,8 +1,16 @@
+import os
 from rest_framework.permissions import SAFE_METHODS, BasePermission
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 
 class IsAdminOrIfAuthenticatedReadOnly(BasePermission):
     def has_permission(self, request, view):
+        if os.getenv("PERMISSIONS_OFF") is True:
+            return True
+
         return bool(
             (
                 request.method in SAFE_METHODS
