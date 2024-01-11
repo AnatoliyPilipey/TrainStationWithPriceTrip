7475347340d178e7c8a86504683b4d41e82cfe71
Anatoliy Pilipey <pilipeytolya@gmail.com>
2024-01-11 10:10:15 +0200

    Add throttle in settings.py

--- taskmanagement/settings.py
+++ taskmanagement/settings.py
@@ -139,7 +139,15 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
-    )
+    ),
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle"
+    ],
+    "DEFAULT_THROTTLE_RATES": {
+        "anon": "10/minutes",
+        "user": "100/minutes"
+    },
 }
 
 SIMPLE_JWT = {
