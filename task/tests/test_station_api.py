3f157233d63b6051b9d1ee5ba29c184b56f6764d
Anatoliy Pilipey <pilipeytolya@gmail.com>
2024-01-17 13:21:35 +0200

    Add UnauthenticatedStationApiTests for model Station

---
+++ task/tests/test_station_api.py
@@ -0,0 +1,18 @@
+
+from django.test import TestCase
+from django.urls import reverse
+
+from rest_framework.test import APIClient
+from rest_framework import status
+
+
+STATION_URL = reverse("task:station-list")
+
+
+class UnauthenticatedMovieApiTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_auth_required(self):
+        res = self.client.get(STATION_URL)
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
